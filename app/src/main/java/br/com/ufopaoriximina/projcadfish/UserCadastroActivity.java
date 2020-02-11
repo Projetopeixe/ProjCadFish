4f2cc0c0a94d06245af5a2d8cdb1ffe7be49393b
Luis Vitor <vitinhoveras@gmail.com>
2020-02-11 15:05:26 -0300

    Abas

--- app/src/main/java/br/com/ufopaoriximina/projcadfish/UserCadastroActivity.java
+++ app/src/main/java/br/com/ufopaoriximina/projcadfish/UserCadastroActivity.java
@@ -1,14 +1,44 @@
 package br.com.ufopaoriximina.projcadfish;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+import br.com.ufopaoriximina.projcadfish.Fragement.GuiaFragment;
+import br.com.ufopaoriximina.projcadfish.Fragement.PescadorFragment;
+
 public class UserCadastroActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_cadastro);
+
+        smartTabLayout = findViewById(R.id.viewPagertab);
+        viewPager = findViewById(R.id.viewpager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Guia", GuiaFragment.class)
+                        .add("Pescador", PescadorFragment.class)
+                .create()
+        );
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewPagertab);
+        viewPagerTab.setViewPager(viewPager);
+
     }
 }
