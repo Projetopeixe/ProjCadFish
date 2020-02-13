8f775513b37cdae232ddd6796e05be51d6da3444
Luis Vitor <vitinhoveras@gmail.com>
2020-02-13 16:22:09 -0300

    BtProx

--- app/src/main/java/br/com/ufopaoriximina/projcadfish/Fragement/GuiaFragment.java
+++ app/src/main/java/br/com/ufopaoriximina/projcadfish/Fragement/GuiaFragment.java
@@ -1,6 +1,7 @@
 package br.com.ufopaoriximina.projcadfish.Fragement;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import br.com.ufopaoriximina.projcadfish.R;
 
@@ -21,12 +23,25 @@ public class GuiaFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private Button btProx;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guia2, container, false);
+        View view = inflater.inflate(R.layout.fragment_guia2, container, false);
+        btProx = view.findViewById(R.id.btProx);
+
+        btProx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), TestFragment.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
+
+
 
 }
