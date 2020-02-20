8287875a545ce3b4aeb09dd255c405dc9d8e7bcb
Samuel <samuelamorim865@gmail.com>
2020-02-20 16:50:48 -0300

    Changes in Activities

--- app/src/main/java/br/com/ufopaoriximina/projcadfish/activity/ActivityInicial.java
+++ app/src/main/java/br/com/ufopaoriximina/projcadfish/activity/ActivityInicial.java
@@ -4,19 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 import br.com.ufopaoriximina.projcadfish.R;
 
-public class ActivityCarregamento extends AppCompatActivity {
+public class ActivityInicial extends AppCompatActivity {
+
+    private ImageView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_carregamento);
+        setContentView(R.layout.activity_inicial);
         getSupportActionBar().hide();
 
+        login = findViewById(R.id.btn_login_inicial);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abriTelaInicialLogin();
+            }
+        });
     }
 
     public void abriTelaInicialLogin(){
