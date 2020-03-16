6d43870c62b06ba810e07b6c6850ca6571e11dc5
Samuel <samuelamorim865@gmail.com>
2020-03-16 15:22:35 -0300

    DataModel

---
+++ app/src/main/java/br/com/ufopaoriximina/projcadfish/model/GrupoPerfil.java
@@ -0,0 +1,23 @@
+package br.com.ufopaoriximina.projcadfish.model;
+
+public class GrupoPerfil {
+
+    private int id;
+    private String data_criacao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getData_criacao() {
+        return data_criacao;
+    }
+
+    public void setData_criacao(String data_criacao) {
+        this.data_criacao = data_criacao;
+    }
+}
