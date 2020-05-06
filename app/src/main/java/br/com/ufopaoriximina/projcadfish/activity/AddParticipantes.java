3afad98850f85a1399a8cf1630ab5c51db92fa48
Luis Vitor <vitinhoveras@gmail.com>
2020-05-06 15:58:07 -0300

    java

---
+++ app/src/main/java/br/com/ufopaoriximina/projcadfish/activity/AddParticipantes.java
@@ -0,0 +1,77 @@
+package br.com.ufopaoriximina.projcadfish.activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import br.com.ufopaoriximina.projcadfish.R;
+
+public class AddParticipantes extends AppCompatActivity {
+
+    ListView listView;
+    String mNome[] = {"João Silva", "Mário Souza"};
+    int images[] = {R.drawable.icon_individual, R.drawable.peixe};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_participantes);
+        getSupportActionBar().hide();
+
+        listView = findViewById(R.id.listView);
+
+        MyAdapter adapter = new MyAdapter(this, mNome , images);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position ==  0) {
+                    Toast.makeText(AddParticipantes.this, "", Toast.LENGTH_SHORT).show();
+                }
+                if (position ==  0) {
+                    Toast.makeText(AddParticipantes.this, "", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rNome[];
+        int rImgs[];
+
+        MyAdapter(Context c, String Nome[], int Imgs[]) {
+            super(c, R.layout.row, R.id.textView1, Nome);
+            this.context = c;
+            this.rNome = Nome;
+            this.rImgs = Imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row_participantes, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myNome = row.findViewById(R.id.textView1);
+
+            images.setImageResource(rImgs[position]);
+            myNome.setText(rNome[position]);
+
+            return row;
+        }
+    }
+
+}
+
