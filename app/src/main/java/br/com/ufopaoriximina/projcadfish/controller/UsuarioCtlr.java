42abef128cdbf9320e7c281c9a2b970718bd5274
Luis Vitor <vitinhoveras@gmail.com>
2020-06-06 00:40:23 -0300

    controller

---
+++ app/src/main/java/br/com/ufopaoriximina/projcadfish/controller/UsuarioCtlr.java
@@ -0,0 +1,24 @@
+package br.com.ufopaoriximina.projcadfish.controller;
+
+import java.util.List;
+
+import br.com.ufopaoriximina.projcadfish.dao.DataSource;
+import br.com.ufopaoriximina.projcadfish.dao.ParticipanteDAO;
+import br.com.ufopaoriximina.projcadfish.model.Usuario;
+
+public class UsuarioCtlr {
+
+    private final ParticipanteDAO participanteDAO;
+
+    public UsuarioCtlr(DataSource pDataSource){
+        participanteDAO = new ParticipanteDAO(pDataSource);
+    }
+
+    public long salvarUsuarioCtrl(Usuario pUsuario){
+      return this.participanteDAO.salvarParticipanteDAO(pUsuario);
+    }
+
+    public List<Usuario> getListaUsuarioCtrl(){
+        return this.participanteDAO.getListaParticipanteDAO();
+    }
+}
