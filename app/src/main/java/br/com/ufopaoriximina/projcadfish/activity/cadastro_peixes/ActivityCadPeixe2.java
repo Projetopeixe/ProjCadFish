af4250b748266257c79716d695bf620d2eb95790
Samuel <samuelamorim865@gmail.com>
2020-06-26 17:41:07 -0300

    Initial register of fish

---
+++ app/src/main/java/br/com/ufopaoriximina/projcadfish/activity/cadastro_peixes/ActivityCadPeixe2.java
@@ -0,0 +1,16 @@
+package br.com.ufopaoriximina.projcadfish.activity.cadastro_peixes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.ufopaoriximina.projcadfish.R;
+
+public class ActivityCadPeixe2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_peixe2);
+    }
+}
---
+++ app/src/main/java/br/com/ufopaoriximina/projcadfish/activity/cadastro_peixes/ActivityCadPeixe3.java
@@ -0,0 +1,16 @@
+package br.com.ufopaoriximina.projcadfish.activity.cadastro_peixes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.ufopaoriximina.projcadfish.R;
+
+public class ActivityCadPeixe3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_peixe3);
+    }
+}
